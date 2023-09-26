f743c4c1cb585731b045a17c515c6a41f2d2cf10
ArunKumarRavi13 <122594237+ArunKumarRavi13@users.noreply.github.com>
2023-09-26 12:53:59 +0530

    Add files via upload

---
+++ DotNetSevenGrid/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetSevenGrid
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DotNetSevenGrid/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using Path = Microsoft.Maui.Controls.Shapes.Path;
+
+namespace DotNetSevenGrid
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            AddPath();
+        }
+
+        void AddPath()
+        {
+            grid.VerticalOptions = LayoutOptions.Center;
+            grid.HorizontalOptions = LayoutOptions.Center;
+            Path squareFillandStrokePath = GetPath("M 10,100 L 100,100 100,50Z");
+            squareFillandStrokePath.Fill = new SolidColorBrush(Colors.Yellow);
+            squareFillandStrokePath.Stroke = new SolidColorBrush(Colors.Black);
+            grid.Children.Add(squareFillandStrokePath);
+        }
+
+        Path GetPath(string pathData)
+        {
+            Path path = new Path();
+            var data = new PathGeometryConverter().ConvertFromString(pathData);
+            path.Data = data as Geometry;
+            return path;
+        }
+
+
+    }
+}
